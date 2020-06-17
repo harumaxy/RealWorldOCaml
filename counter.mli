07e72c6bd12876ca974d16bc9ffaf08ab6897ce3
max <harumaxy@gmail.com>
2020-06-17 16:55:45 +0900

    update

--- counter.mli
+++ counter.mli
@@ -6,7 +6,3 @@ val empty : t
 val touch : t -> string -> t
 
 val to_list : t -> (string * int) list
-
-include module type of Option
-
-val apply : ('a -> 'b) t -> 'a -> 'b t
--- main.ml
+++ main.ml
@@ -1,14 +1,31 @@
-module Interval = struct
-  type t = Interval of int * int | Empty
+open Core
+open Base
 
-  let create low high = if high < low then Empty else Interval (low, high)
+type 'a with_line_num = { item : 'a; line_num : int }
+
+module Heartbeat = struct
+  type t = { session_id : string; time : Time_ns.t; status_message : string }
 end
 
-module Extended_interval = struct
-  include Interval
+type client_info = {
+  addr : Unix.Inet_addr.t;
+  port : int;
+  user : string;
+  credentials : string;
+  last_heartbeat_time : Time_ns.t;
+}
 
-  let contains t x =
-    match t with
-    | Empty -> false
-    | Interval (low, high) -> x >= low && x <= high
+let register_heartbeat t hb = { t with last_heartbeat_time = hb.Heartbeat.time }
+
+module Logon = struct
+  type t = {
+    session_id : string;
+    time : Time_ns.t;
+    user : string;
+    credentials : string;
+  }
+  [@@deriving fields]
 end
+
+;;
+Field.get Logon.Field.user
