c8522611fcdc13a580f6f3eb9633375bbdc1bef8
max <harumaxy@gmail.com>
2020-06-22 16:43:51 +0900

    section7

--- main.ml
+++ main.ml
@@ -1,53 +1,71 @@
 open Base
+
+(* 'a list -> f:('a -> bool) -> 'a option *)
+
+(* let compute_bounds ~compare list =
+  let sorted = List.sort ~compare list in
+  match (List.hd sorted, List.last sorted) with
+  | None, _ | _, None -> None
+  | Some x, Some y -> Some (x, y) *)
+
+(* こちらはエラーの概念がなく、キーを見つけられなかったとしても空のmismatchが帰るだけ *)
+(* ただし、見つからなかった場合がエラーなのか正しいのか、戻り値からは判別しにくい　 *)
+let find_mismatches table1 table2 =
+  Hashtbl.fold table1 ~init:[] ~f:(fun ~key ~data mismatches ->
+      match Hashtbl.find table2 key with
+      | Some data' when data' <> data -> key :: mismatches
+      | _ -> mismatches)
+
+let custom_to_sexp = [%sexp_of: float * string list * int]
+
+let e =
+  Error.create "Something went terribly wrong"
+    (3.5, [ "a"; "b"; "c" ], 6034)
+    [%sexp_of: float * string list * int]
+
+(* let bind option f =
+  match option with 
+  | None -> None 
+  | Some x -> f x
+   *)
+
+(* bind >>= 演算子を使って書き直す、最大値と最小値の抽出 *)
+
+(* let compute_bounds ~compare list =
+  let open Option.Monad_infix in
+  let sorted = List.sort ~compare list in
+  List.hd sorted >>= fun first ->
+  List.last sorted >>= fun last -> Some (first, last) *)
+
+(* ラムダ式で、見つけた値を取る *)
+(* どこかで失敗したらNoneが戻る *)
+(* let compute_bounds ~compare list =
+  let open Option.Monad_infix in
+  let sorted = List.sort ~compare list in
+  List.hd sorted >>= fun first ->
+  List.last sorted >>= fun last -> Some (first, last) *)
+
+(* 
+let compute_bounds ~compare list =
+  let open Option.Let_syntax in 
+  let sorted = List.sort ~compare list in
+  let%bind first = List.hd sorted in 
+  let%bind last = List.last sorted in 
+  Some (first, last) *)
+
+let compute_bounds ~compare list =
+  let sorted = List.sort ~compare list in
+  Option.both (List.hd sorted) (List.last sorted)
+
+exception Key_not_found of string
+
 open Stdio
 
-type basic_color =
-  | Black
-  | Red
-  | Green
-  | Yellow
-  | Blue
-  | Magenta
-  | Cyan
-  | White
+let parse_line line =
+  String.split_on_chars ~on:[ ',' ] line |> List.map ~f:Float.of_string
 
-;;
-[ Blue; Magenta; Cyan ]
-
-let basic_color_to_int = function
-  | Black -> 0
-  | Red -> 1
-  | Green -> 2
-  | Yellow -> 3
-  | Blue -> 4
-  | Magenta -> 5
-  | Cyan -> 6
-  | White -> 7
-
-;;
-List.map ~f:basic_color_to_int [ Blue; Magenta; Cyan ]
-
-let color_by_number number text =
-  Printf.sprintf "\027[38;5;%dm%s\027[0m" number text
-
-let blue = color_by_number (basic_color_to_int Blue) "Blue"
-
-type weight = Regular | Bold
-
-type color =
-  | Basic of basic_color * weight
-  | RGB of int * int * int
-  | Gray of int
-
-;;
-[ RGB (255, 0, 0); Basic (Green, Regular) ]
-
-type 'a expr =
-  | Base of 'a
-  | Const of bool
-  | And of 'a expr list
-  | Or of 'a expr list
-  | Not of 'a expr
-
-type mail_field = To | From | CC | Date | Subject
-type mail_predicate = { field : mail_field; contains : string }
+let load filename =
+  let inc = In_channel.create filename in
+  let data = In_channel.input_lines inc |> List.map ~f:parse_line in
+  In_channel.close inc;
+  data List.Assoc.find List.Assoc.find_exn
